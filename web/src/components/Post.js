9e3c83def8bf4b7e3e7fc20a3a845bc5fce54dec
claracampos <camposclr@gmail.com>
2020-04-20 16:43:16 -0300

    made author's name a button that filters posts by that author

--- web/src/components/Post.js
+++ web/src/components/Post.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import createPostSummary from '../utils/createPostSummary';
+import AppContext from '../context/AppContext';
 
 const Post = (props) => {
   const { title, body, author, publishedAt } = props.post;
   const summary = createPostSummary(body);
+
+  const { setFilter } = useContext(AppContext);
   const [showPost, setShowPost] = useState(false);
 
   return (
     <div>
       <p onClick={() => setShowPost(!showPost)}>{title}</p>
-      <p>{author.name}</p>
+      <button onClick={() => setFilter({ name: author.name, id: author.id })}>
+        {author.name}
+      </button>
       <p>{publishedAt}</p>
       <p>summary: {summary}...</p>
       {showPost && <p>{body}</p>}
