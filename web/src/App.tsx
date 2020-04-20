79ed590a96961510bde242279da1cef3dd3b2a11
claracampos <camposclr@gmail.com>
2020-04-20 15:28:43 -0300

    added API call inside useEffect hook

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 export function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await axios.get('http://localhost:4000/posts');
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  });
+
   return (
     <div className="App">
       <h1>App</h1>
