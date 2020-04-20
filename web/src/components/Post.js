4bc4bd8b936341ad4c46f56685d701eae6b81993
claracampos <camposclr@gmail.com>
2020-04-20 16:38:47 -0300

    added summary

--- web/src/components/Post.js
+++ web/src/components/Post.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import createPostSummary from '../utils/createPostSummary';
 
 const Post = (props) => {
   const { title, body, author, publishedAt } = props.post;
+  const summary = createPostSummary(body);
   const [showPost, setShowPost] = useState();
 
   return (
@@ -9,6 +11,7 @@ const Post = (props) => {
       <p>{title}</p>
       <p>{author.name}</p>
       <p>{publishedAt}</p>
+      <p>summary: {summary}...</p>
       <p>{body}</p>
     </div>
   );
