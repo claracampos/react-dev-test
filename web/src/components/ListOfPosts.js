56b5f8804d31dc6642b3e5d6afcb45224244491e
claracampos <camposclr@gmail.com>
2020-04-20 16:29:44 -0300

    removed props, getting posts from context instead

--- web/src/components/ListOfPosts.js
+++ web/src/components/ListOfPosts.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Post from './Post';
+import AppContext from '../context/AppContext';
 
-const ListOfPosts = (props) => {
-  const posts = props.posts;
+const ListOfPosts = () => {
+  const { posts } = useContext(AppContext);
 
   return (
     <div>
       {posts.map((post) => (
-        <Post post={post} />
+        <Post post={post} key={post.id} />
       ))}
     </div>
   );
